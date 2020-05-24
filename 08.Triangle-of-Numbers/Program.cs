a5b80b8567c0571829f57e5f6c5e8589ed5b45b7
Serbmusic <gex_15@abv.bg>
2020-05-25 00:36:22 +0300

    Add files via upload

---
+++ 08.Triangle-of-Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08.Triangle_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                Console.WriteLine();
+                for (int j = 1; j <= i ; j++)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+        }
+    }
+}
