8f1c2ecc086247d58dfb78d107b8ea832c23adbb
Serbmusic <gex_15@abv.bg>
2020-05-25 00:32:21 +0300

    Add files via upload

---
+++ 03.Vacation/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfPeople = int.Parse(Console.ReadLine());
+            string typeOfPerson = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            
+            double price = 0;
+
+            switch (dayOfWeek)
+            {
+                case "Friday":
+                    switch (typeOfPerson)
+                    {
+                        case "Students":price = 8.45;
+                            break;                        
+                        case "Business":price = 10.90;
+                            break;                        
+                        case "Regular":price = 15;
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                    switch (typeOfPerson)
+                    {
+                        case "Students":price = 9.80;
+                            break;
+                        case "Business":price = 15.60;
+                            break;
+                        case "Regular":price = 20;
+                            break;
+                    }
+                    break;
+                case "Sunday":
+                    switch (typeOfPerson)
+                    {
+                        case "Students":price = 10.46;
+                            break;
+                        case "Business":price = 16;
+                            break;
+                        case "Regular":price = 22.50;
+                            break;
+                    }
+                    break;
+            }
+            if (typeOfPerson == "Students" && countOfPeople >= 30)
+            {
+                Console.WriteLine($"Total price: {price * countOfPeople * 0.85:f2}");
+            }
+            else if (typeOfPerson == "Business" && countOfPeople >= 100)
+            {
+                Console.WriteLine($"Total price: {(price * countOfPeople) - (price * 10):f2}");
+            }
+            else if (typeOfPerson == "Regular" && countOfPeople >= 10 && countOfPeople <= 20)
+            {
+                Console.WriteLine($"Total price: {price * countOfPeople * 0.95:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"Total price: {price * countOfPeople:f2}");
+            }
+        }
+    }
+}
---
+++ 04.Print-and-sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.Print_and_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1from = int.Parse(Console.ReadLine());
+            int num2to = int.Parse(Console.ReadLine());
+            int sumNum = 0;
+
+            for (int i = num1from; i <= num2to ; i++)
+            {
+                int currentNum = i;
+                Console.Write($"{currentNum} ");
+                sumNum += currentNum;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sumNum}");
+        }
+    }
+}
